ca5b164d07658d48cb0ffb64ada550bf8e2736b2
Anshika Tyagi <53554113+anshika-hash@users.noreply.github.com>
2022-01-22 17:38:00 +0700

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://Anshika770:5433728a@cluster0.rronz.mongodb.net/Profile?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true,useFindAndModify:false})
+mongoose.connect('mongodb+srv://Anshika770:5433728a@cluster0.rronz.mongodb.net/Profile?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true})
 .then(()=>{
     console.log("connection is sucessful");
 }).catch((e)=>{
     console.log('Not connected');
 });
+
+//useCreateIndex:true,useFindAndModify:false
